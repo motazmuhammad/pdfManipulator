867506806f80d14d809f0dc90fad8c8661e1df27
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2020-11-21 21:55:38 +0200

    implementation of the openFile button

--- notePad/mainwindow.cpp
+++ notePad/mainwindow.cpp
@@ -14,3 +14,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file ");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly|QFile::Text))
+    {
+
+        QMessageBox::warning(this, "can't open file ",file.errorString());
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
