ded2007499db110e0c475ec3025e24a63fe25d09
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2020-11-21 21:13:21 +0200

    add headers - add variable for the filename

--- notePad/mainwindow.h
+++ notePad/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,9 +19,12 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    QString currentFile="";
+
 };
 #endif // MAINWINDOW_H
